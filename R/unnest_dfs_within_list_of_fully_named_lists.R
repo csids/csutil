5c11116667ff7f74a09a91eda7f2859eaa23fb20
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-04-28 09:06:36 +0200

    renaming

--- R/unnest_dfs_within_list_of_fully_named_lists.R
+++ R/unnest_dfs_within_list_of_fully_named_lists.R
@@ -1,6 +1,6 @@
 #' Unnest data.frames within fully named list
 #' @param x A list of fully named lists (which then contain data.frames)
-#' @param returned_name_when_dfs_are_unnested When x is a single list of data.frames, what name should be returned?
+#' @param returned_name_when_dfs_are_not_nested When x is a single list of data.frames, what name should be returned?
 #' @param ... parameters passed to data.table::rbindlist
 #' @examples
 #' x <- list(
@@ -19,20 +19,20 @@
 #'   NULL
 #' )
 #' print(x)
-#' splutil::unnest_dfs_within_fully_named_list(x)
+#' splutil::unnest_dfs_within_list_of_fully_named_lists(x)
 #'
 #' x <- list(
 #'   data.frame("v1"=1),
 #'   data.frame("v3"=50)
 #' )
 #' print(x)
-#' splutil::unnest_dfs_within_fully_named_list(x, returned_name_when_dfs_are_unnested = "NAME", fill = T)
+#' splutil::unnest_dfs_within_list_of_fully_named_lists(x, returned_name_when_dfs_are_not_nested = "NAME", fill = T)
 #' @export
-unnest_dfs_within_fully_named_list <- function(x, returned_name_when_dfs_are_unnested = "data", ...){
+unnest_dfs_within_list_of_fully_named_lists <- function(x, returned_name_when_dfs_are_not_nested = "data", ...){
   if(!inherits(x, "list")) return(NULL)
   if(all_list_elements_null_or_df(x)){
     retval <- list(rbindlist(x, ...))
-    names(retval) <- returned_name_when_x_is_unnested
+    names(retval) <- returned_name_when_dfs_are_not_nested
     return(retval)
   }
   if(!all_list_elements_null_or_fully_named_list(x)) stop("All list elements must be either null or a fully named list")
